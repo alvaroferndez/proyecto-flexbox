fda9b77b74d926270e15c3f5c39b74e9e0976316
alvaro fernandez domingo <alvarofdayala@gmail.com>
2022-11-24 10:48:45 +0100

    aside transicion completa

--- index.js
+++ index.js
@@ -12,40 +12,11 @@ window.addEventListener("load", () => {
       secciones_div = document.getElementById("contenedor-secciones");
       secciones = document.getElementsByClassName("secciones");
 
-      function abrirMenu(){
-            lineas = document.getElementsByClassName("linea");
-            if(abrir == true){
-                  menu.style.backgroundColor = "#FFCF03";
-                  secciones_div.style.opacity = "0.6";
-                  header.style.height = "15vh";
-                  nav.style.visibility = "visible";
-                  for(let i=0; i<3; i++){
-                        lineas[0].className = "linea_abierta";
-                  }
-                  abrir = false;
-            }else{
-                  menu.style.backgroundColor = "white";
-                  secciones_div.style.opacity = "1";
-                  header.style.height = "5vh";
-                  nav.style.visibility = "hidden";
-                  lineas = document.getElementsByClassName("linea_abierta");
-                  for(let i=0; i<3; i++){
-                        lineas[0].className = "linea";
-                  }
-                  abrir = true;
-            }
 
-      }
-      
-      function manejoSecciones(e,i){      
-            if (e.target.alt == secciones[i].id){
-                  for (seccion of secciones){
-                        seccion.style.display = "none";
-                  }
-                  secciones[i].style.display = "block";
-            }
-            menu.style.backgroundColor = "white";
-      }
+      //aside
+      flecha = document.getElementById("flecha");
+      contenedor_aside = document.getElementById("contenedor-aside");
+      aside_global = document.getElementById("aside-global");
 
       menu.addEventListener("click", abrirMenu);
       for (let i=0; i<imagenes.length; i++){
@@ -61,4 +32,68 @@ window.addEventListener("load", () => {
                   abrir = true;
             })
       }
+
+      flecha.addEventListener("click", abrirAside)
 })
+
+function abrirMenu(){
+      lineas = document.getElementsByClassName("linea");
+      if(abrir){
+            menu.style.backgroundColor = "#FFCF03";
+            secciones_div.style.opacity = "0.6";
+            header.style.height = "15vh";
+            nav.style.visibility = "visible";
+            for(let i=0; i<3; i++){
+                  lineas[0].className = "linea_abierta";
+            }
+            abrir = false;
+      }else{
+            menu.style.backgroundColor = "white";
+            secciones_div.style.opacity = "1";
+            header.style.height = "5vh";
+            nav.style.visibility = "hidden";
+            lineas = document.getElementsByClassName("linea_abierta");
+            for(let i=0; i<3; i++){
+                  lineas[0].className = "linea";
+            }
+            abrir = true;
+      }
+
+}
+
+function manejoSecciones(e,i){      
+      if (e.target.alt == secciones[i].id){
+            for (seccion of secciones){
+                  seccion.style.display = "none";
+            }
+            secciones[i].style.display = "block";
+      }
+      menu.style.backgroundColor = "white";
+}
+
+function abrirAside(){
+      lineas = document.getElementsByClassName("linea-flecha");
+      if(abrir){
+            secciones_div.style.opacity = "0.4";
+            aside_global.style.opacity = "1";
+            flecha.style.backgroundColor = "#ffd52f00";
+            contenedor_aside.style.zIndex = "29";
+            contenedor_aside.style.width = "15vw";
+            for(let i=0; i<2; i++){
+                  lineas[0].className = "linea-flecha-reves";
+            }
+            abrir = false;
+      }else{
+            secciones_div.style.opacity = "1";
+            aside_global.style.opacity = "0";
+            flecha.style.backgroundColor = "rgba(193, 193, 193, 0.411)";
+            contenedor_aside.style.zIndex = "0";
+            contenedor_aside.style.width = "10vw";
+            lineas = document.getElementsByClassName("linea-flecha-reves");
+            for(let i=0; i<2; i++){
+                  lineas[0].className = "linea-flecha";
+            }
+            abrir = true;
+      }
+
+}
